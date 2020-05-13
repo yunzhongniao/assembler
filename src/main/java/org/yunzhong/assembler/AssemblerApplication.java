3883a0ffb36e79ed88f1074ae1b5183336effba2
wangyunzhong <wangyunzhong@aliyun.com>
2020-05-13 16:29:54 +0800

    init

---
+++ src/main/java/org/yunzhong/assembler/AssemblerApplication.java
@@ -0,0 +1,13 @@
+package org.yunzhong.assembler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yunzhong/assembler/AssemblerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yunzhong.assembler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
